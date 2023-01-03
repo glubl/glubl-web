a65ed49a2ada950015432691d15552d20bcdc6ac
dirtboll <dirtboll.boi@gmail.com>
2023-01-03 23:04:31 +0700

    feat: add login and register page

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,10 @@
+<div class="card w-96 bg-base-300 shadow-xl backdrop-blur">
+  <div class="card-body prose prose-sm sm:prose-base gap-y-3">
+    <h1>Register</h1>
+    <input type="text" placeholder="Username" class="input w-full max-w-xs" />
+    <input type="password" placeholder="Password" class="input w-full max-w-xs" />
+    <input type="password" placeholder="Confirm Password" class="input w-full max-w-xs" />
+    <button class="btn btn-accent">Submit</button>
+    <p class="place-self-end">Have an account? <a data-sveltekit-preload-code="hover" href="/login">Login</a></p>
+  </div>
+</div>
