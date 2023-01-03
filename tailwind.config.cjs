c962287d1141c1432fba215c800ed1e2094db092
dirtboll <dirtboll.boi@gmail.com>
2023-01-03 19:14:52 +0700

    chore: add gun

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,6 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/line-clamp"),
-    require('tailwindcss-filters'),
     require("daisyui"),
     require("prettier-plugin-tailwindcss")
   ],
