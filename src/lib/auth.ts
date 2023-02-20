02585e1979408137a4b4724e23820d14ee31d268
dirtboll <61202885+dirtboll@users.noreply.github.com>
2023-02-20 23:35:16 +0700

    feat: key pair verification (#5)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,9 @@
-import gun from "@lib/initGun"
+import { gun } from "@lib/initGun"
+
 import SEA from "gun/sea"
 import type { Profile } from "./types"
 import type { ISEAPair } from "gun"
 import { InvalidPairError } from "./errors"
-
-export const user = gun.user()
 
 const auth = {
     /**
@@ -15,8 +14,10 @@ const auth = {
     async login(stringKeyPair: string) {
         try {
             const keyPair = JSON.parse(stringKeyPair)
-            const res = user.auth(keyPair)
-            if (SEA.err || res.ing) {
+            await this.verifyPair(keyPair)
+            let authResp: any;
+            const res = gun.user().auth(keyPair, (o) => authResp = o)
+            if (authResp.err || res.ing) {
                 throw new InvalidPairError()
             }
             const isUser = res.is !== undefined
@@ -30,9 +31,8 @@ const auth = {
         } catch (error: any) {
             if (error.name === "SyntaxError") {
                 throw new InvalidPairError()
-            } else {
-                throw Error("Error")
             }
+            throw error
         }
     },
 
@@ -45,12 +45,12 @@ const auth = {
         const stringPair : string = JSON.stringify(pair).replace(/\%22/g,'"')
         await this.login(stringPair)
         const userProfile: Profile = {'picture': '','username': username,'pub': pair.pub}
-        gun.get('users')
-            .get('~'+pair.pub)
-            .put({contacts: null})
-            .put({messages: null})
-            .put({settings: null})
-            .put({profile: JSON.stringify(userProfile)})
+        gun.user()
+          .put({contacts: null})
+          .put({messages: null})
+          .put({settings: null})
+          .get({profile: userProfile})
+          .put({'picture': '','username': username,'pub': pair.pub})
         return stringPair
     },
 
@@ -58,10 +58,24 @@ const auth = {
         localStorage.clear()
         location.hash = ""
         location.assign("/login")
+        const user = gun.user()
+        user.leave();
+        (user._ as any).sea = undefined
         // todo more cleanup if necessary
     },
 
-
+    async verifyPair(pair: ISEAPair) {
+      let err = "Invalid key-pair!"
+      let msg = "DINGUS123"
+      let enc = await SEA.encrypt(msg, pair)
+      if (!enc) throw err
+      let sig = await SEA.sign(enc, pair)
+      if (!sig) throw err
+      enc = await SEA.verify(sig, pair)
+      if (!enc) throw err
+      let msg2 = await SEA.decrypt(enc, pair)
+      if (msg !== msg2) throw err
+    }
 }
 
 export function protectedRedirect() {
