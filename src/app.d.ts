a8679de02c89eca20375b5ce13fd13213cb087eb
dirtboll <dirtboll.boi@gmail.com>
2023-03-22 16:51:45 +0700

    refactor: typing

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,71 +1,83 @@
-declare type User = {
-  profilePicture: string
-  name: string
-  id: string
-  pubKey: string
-  lastMessageTs: number
+import { _GunRoot } from "gun"
+
+declare global {
+  type User = {
+    profilePicture: string
+    name: string
+    id: string
+    pubKey: string
+    lastMessageTs: number
+  }
+  
+  type Message = {
+    message: string
+    ts: number
+    by: User
+  }
+  
+  type MenuScreen = {
+    name: string
+    icon: IconSource
+    menuComponent?: ComponentType
+    contentComponent?: ComponentType
+    type: "top" | "bottom"
+  }
+  
+  type Profile = {
+    pub: string
+    epub: string
+    picture: string
+    username: string
+  }
+  
+  type FriendProfile = Profile & {
+    space: string
+  }
+  
+  type FriendRequest = {
+    pub: string
+    epub: string
+    profile: Profile
+  }
+  
+  type ChatMessage = {
+    msg: string
+    ts: number
+    by: FriendProfile
+    to: FriendProfile
+  }
+  
+  type ChatMessageGun = {
+    msg: string
+    ts: number
+    by: string
+    to: string
+  }
+
+  interface String {
+    toTitleCase(): string;
+  }
+
+  interface Window {
+    SEA: ISEA;
+    gun: IGunInstance<any>
+  }
 }
 
-declare type Message = {
-  message: string
-  ts: number
-  by: User
+declare module "gun" {
+  interface _GunRoot {
+    sea?: ISEAPair;
+    $: { _: _GunRoot };
+  
+    /**
+     * Current GUN options
+     */
+    opt: GunOptions;
+  
+    on(event: 'friend', data: {pub: string, epub: string, path: string, mypath: string}): void;
+  }
 }
 
-declare type MenuScreen = {
-  name: string
-  icon: IconSource
-  menuComponent?: ComponentType
-  contentComponent?: ComponentType
-  type: "top" | "bottom"
-}
-
-
-declare type Profile = {
-  pub: string
-  epub: string
-  picture: string
-  username: string
-}
-
-declare type FriendProfile = Profile & {
-  space: string
-}
-
-declare type FriendRequest = {
-  pub: string
-  epub: string
-  profile: Profile
-}
-
-declare type ChatMessage = {
-  msg: string
-  ts: number
-  by: FriendProfile
-  to: FriendProfile
-}
-
-declare type ChatMessageGun = {
-  msg: string
-  ts: number
-  by: string
-  to: string
-}
-
-declare interface _GunRoot extends IGunInstanceHookHandler {
-  sea?: ISEAPair
-  $: { _: _GunRoot };
-
-  /**
-   * Current GUN options
-   */
-  opt: GunOptions;
-}
-
-declare interface Window {
-  SEA: ISEA;
-  gun: IGunInstance<any>
-}
 
 declare type UserDatabase = {
   settings: {
@@ -83,8 +95,4 @@ declare type UserDatabase = {
     picture: string
     username: string
   }
-}
-
-interface String {
-  toTitleCase(): string;
 }
