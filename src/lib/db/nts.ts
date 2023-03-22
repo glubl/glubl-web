5b1cba2fab7e2a14c6563679ca71560b674a1a5f
dirtboll <dirtboll.boi@gmail.com>
2023-03-23 04:42:04 +0700

    refactor: remove console.log

--- src/lib/db/nts.ts
+++ src/lib/db/nts.ts
@@ -18,10 +18,11 @@ let dumbPeers = new Set<string>()
 let pending: {[pid: string]: NTS} = {}
 let mesh: any
 let timeout: number = 2 * 1000
+let interval: ReturnType<typeof setInterval>
 
 function sendNts(peer: GunPeer) {
   return new Promise<NTSResult>((res) => {
-    console.log("start-sendnts")
+    //console.log("start-sendnts")
     let nts: NTS = {
       start: Gun.state(),
       prom: res,
@@ -32,30 +33,30 @@ function sendNts(peer: GunPeer) {
   
     // Handle peers that don't speak NTS
     nts.timeout = setTimeout(() => {
-      console.log("timeout", pending)
+      //console.log("timeout", pending)
       if (peer.id in pending) {
-        console.log("got-dumb-peer", peer)
+        //console.log("got-dumb-peer", peer)
         delete pending[peer.id]
         dumbPeers.add(peer.id)
         res({pid: peer.id, drift: 0})
       }
     }, timeout)
-    console.log("end-sendnts")
+    //console.log("end-sendnts")
   })
 }
 
 function resolveNts(nts: NTS) {
-  console.log("start-resolvents", nts)
+  //console.log("start-resolvents", nts)
   if (!nts.end || !nts.time || !nts.start || !nts.prom) return
   const latency = (nts.end - nts.start) / 2
   const drift = nts.time - nts.end + latency
   clearTimeout(nts.timeout)
   nts.prom({pid: nts.peer.id, drift})
-  console.log("end-resolvents", {pid: nts.peer.id, drift})
+  //console.log("end-resolvents", {pid: nts.peer.id, drift})
 }
 
 function hearNts(msg: NTS, peer: GunPeer) {
-  console.log("start-hearnts", msg, peer)
+  //console.log("start-hearnts", msg, peer)
   if (!msg.time) {
     mesh.say({dam: "nts", time: Gun.state()}, {[peer.id]: peer})
     return
@@ -68,12 +69,12 @@ function hearNts(msg: NTS, peer: GunPeer) {
   nts.end = Gun.state()
   delete pending[peer.id]
   resolveNts(nts)
-  console.log("end-hearnts")
+  //console.log("end-hearnts")
 }
 
 async function doSync(root: _GunRoot) {
   const opt = root.opt;
-  console.log("start-dosync", opt.peers)
+  //console.log("start-dosync", opt.peers)
   if (Object.keys(pending).length > 0) return
   let res = await Promise.all(
     Object.entries<GunPeer>(opt.peers)
@@ -87,7 +88,7 @@ async function doSync(root: _GunRoot) {
   let drift = res.map((a) => a.drift)
     .reduce((a, b) => a + b) / res.length
   Gun.state.drift += drift
-  console.log("end-dosync", drift)
+  //console.log("end-dosync", drift)
 } 
 
 
@@ -96,7 +97,7 @@ Gun.on("opt", function init(root: _GunRoot) {
   let opt = root.opt;
   mesh = opt.mesh = opt.mesh || Gun.Mesh(root);
   mesh.hear.nts = hearNts
-  setInterval(() => {
+  interval = setInterval(() => {
     doSync(root)
   }, root.opt.ntsInterval || 5 * 1000)
   doSync(root)
