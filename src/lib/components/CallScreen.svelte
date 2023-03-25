187e98f33e511251059a8835ae4c5f3c92d381b8
Niti Utami <n.c.utami@gmail.com>
2023-03-25 13:45:26 +0700

    fix: media settings layout
    change drawer to modal

--- src/lib/components/CallScreen.svelte
+++ src/lib/components/CallScreen.svelte
@@ -162,95 +162,96 @@
   });
 </script>
 
-<div class="drawer drawer-end">
-  <input id="media-settings" type="checkbox" class="drawer-toggle" />
+<div
+  class="flex flex-col flex-1 space-y-2 ${fullScreen
+    ? ''
+    : 'bg-base-300 w-full h-60 py-2'}"
+>
+  {#if fullScreen}
+    <div
+      id="header"
+      class="flex flex-row items-center h-14 py-4 px-2 backdrop-blur shadow-lg bg-base-200"
+    >
+      <NavButton />
+    </div>
+  {/if}
+
+  <!-- Room participants tiles -->
   <div
-    class="drawer-content flex flex-col flex-1 space-y-2 ${fullScreen
-      ? ''
-      : 'bg-base-300 w-full h-60 py-2'}"
+    class="px-4 {fullScreen
+      ? 'place-self-center flex-1 grid md:grid-cols-3 grid-cols-2 md:auto-rows-[200px] auto-rows-[120px] gap-2 overflow-x-hidden overflow-y-auto'
+      : 'flex flex-row h-full w-full space-x-2 overflow-x-auto'}"
   >
-    {#if fullScreen}
+    {#if myProfile}
+      <ProfileTile profile={myProfile} {myProfile} />
+    {/if}
+    {#each Object.entries(calleeProfiles).slice(0, profileShow) as [key, profile]}
+      <ProfileTile {profile} />
+    {/each}
+
+    {#if Object.entries(calleeProfiles).length > profileShow}
       <div
-        id="header"
-        class="flex flex-row items-center h-14 py-4 px-2 backdrop-blur shadow-lg bg-base-200"
+        class="flex flex-col justify-center items-center rounded-md bg-base-200"
       >
-        <NavButton />
+        <strong class="prose w-48 text-center">
+          + {Object.entries(calleeProfiles).length - profileShow}
+        </strong>
       </div>
     {/if}
-
-    <div
-      class="px-4 {fullScreen
-        ? 'place-self-center flex-1 grid md:grid-cols-3 grid-cols-2 md:auto-rows-[200px] auto-rows-[120px] gap-2 overflow-x-hidden overflow-y-auto'
-        : 'flex flex-row h-full w-full space-x-2 overflow-x-auto'}"
-    >
-      {#if myProfile}
-        <ProfileTile profile={myProfile} {myProfile} />
-      {/if}
-      {#each Object.entries(calleeProfiles).slice(0, profileShow) as [key, profile]}
-        <ProfileTile {profile} />
-      {/each}
-
-      {#if Object.entries(calleeProfiles).length > profileShow}
-        <div
-          class="flex flex-col justify-center items-center rounded-md bg-base-200"
-        >
-          <strong class="prose w-48 text-center">
-            + {Object.entries(calleeProfiles).length - profileShow}
-          </strong>
-        </div>
-      {/if}
-    </div>
-    <!-- Room participants tiles -->
-
-    <!-- Buttons -->
-    <div
-      class="flex flex-row space-x-4 justify-center {fullScreen
-        ? 'pb-8 pt-2 sticky bottom-0 rounded-t-2xl bg-base-200'
-        : 'rounded-2xl'}"
-    >
-      <button class="btn btn-error btn-circle p-3" on:click={endcall}
-        ><Icon src={Phone} theme="solid" /></button
-      >
-      <button
-        class="btn {isMicOn ? 'btn-accent' : 'btn-error'} btn-circle p-3"
-        on:click={toggleMic}
-        ><Icon src={isMicOn ? Mic : MicOff} theme="solid" /></button
-      >
-      <button
-        class="btn {isCameraOn ? 'btn-accent' : 'btn-error'} btn-circle p-3"
-        on:click={toggleCamera}
-        ><Icon src={isCameraOn ? Video : VideoOff} theme="solid" /></button
-      >
-      <div class="btn btn-accent btn-circle p-3">
-        <label for="media-settings">
-          <Icon src={Settings} />
-        </label>
-      </div>
-    </div>
-    <!-- Buttons -->
   </div>
-  <!-- Side drawer -->
-  <div class="drawer-side">
-    <label for="media-settings" class="drawer-overlay" />
-    <form
-      class="form-group bg-base-200 p-4"
-      on:submit|preventDefault={onSettingsSaved}
+  <!-- Room participants tiles -->
+
+  <!-- Buttons -->
+  <div
+    class="flex flex-row space-x-4 justify-center {fullScreen
+      ? 'pb-8 pt-2 sticky bottom-0 rounded-t-2xl bg-base-200'
+      : 'rounded-2xl'}"
+  >
+    <button class="btn btn-error btn-circle p-3" on:click={endcall}
+      ><Icon src={Phone} theme="solid" /></button
     >
-      <div>
-        <h3>Audio Input</h3>
-        <div bind:this={audioinputGroup} />
+    <button
+      class="btn {isMicOn ? 'btn-accent' : 'btn-error'} btn-circle p-3"
+      on:click={toggleMic}
+      ><Icon src={isMicOn ? Mic : MicOff} theme="solid" /></button
+    >
+    <button
+      class="btn {isCameraOn ? 'btn-accent' : 'btn-error'} btn-circle p-3"
+      on:click={toggleCamera}
+      ><Icon src={isCameraOn ? Video : VideoOff} theme="solid" /></button
+    >
+    <label for="media-settings" class="btn btn-accent btn-circle p-3">
+      <Icon src={Settings} />
+    </label>
+  </div>
+  <!-- Buttons -->
+
+  <input type="checkbox" id="media-settings" class="modal-toggle" />
+  <!-- Modal -->
+  <div class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+      <form
+        class="form-group bg-base-200 p-4"
+        on:submit|preventDefault={onSettingsSaved}
+      >
+        <div>
+          <h3>Audio Input</h3>
+          <div bind:this={audioinputGroup} />
+        </div>
+        <div class="divider" />
+        <div>
+          <h3>Audio Output</h3>
+          <div bind:this={audiooutputGroup} />
+        </div>
+        <div class="divider" />
+        <div>
+          <h3>Video Input</h3>
+          <div bind:this={videoinputGroup} />
+        </div>
+      </form>
+      <div class="modal-action">
+        <label for="media-settings" class="btn btn-accent prose">SAVE</label>
       </div>
-      <div class="divider" />
-      <div>
-        <h3>Audio Output</h3>
-        <div bind:this={audiooutputGroup} />
-      </div>
-      <div class="divider" />
-      <div>
-        <h3>Video Input</h3>
-        <div bind:this={videoinputGroup} />
-      </div>
-      <button class="btn btn-accent prose" type="submit">SAVE</button>
-    </form>
+    </div>
   </div>
 </div>
