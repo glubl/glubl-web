f46fd6c5c600a81ee9f045d3a06351dd23132933
Niti Utami <n.c.utami@gmail.com>
2023-05-27 13:34:59 +0700

    chore: add mock group update data

--- src/lib/mock/groups.ts
+++ src/lib/mock/groups.ts
@@ -6,9 +6,65 @@ function stringToUint8Array(str: string) {
     return ret;
 }
 
+let date = new Date()
+let time = date.toISOString()
+
+type GroupNotification = {
+        groupId: string,
+        groupName: string,
+        createdAt: string,
+        keyEnc: string,
+        sender: string,
+        action: "add" | "update",
+}
+
+export const dummyGroupUpdate: GroupNotification[] = [
+    {
+        groupId: "groupid91",
+        groupName: "Savanna",
+        createdAt: time,
+        keyEnc: "encryptedKey",
+        sender: "treefrog",
+        action: "update",
+    },
+    {
+        groupId: "groupid92",
+        groupName: "Rain Forest",
+        createdAt: time,
+        keyEnc: "encryptedKey",
+        sender: "treefrog",
+        action: "add",
+    },
+    {
+        groupId: "groupid92",
+        groupName: "Rain Forest",
+        createdAt: time,
+        keyEnc: "encryptedKey",
+        sender: "treefrog",
+        action: "update",
+    },
+]
+
+
 const dummyGroups: { [groupId: string]: any } = {
     "groupid91": {
         groupId: stringToUint8Array("groupid91"),
+        state: {
+            members: {
+                "pub-1": {
+                    username: "treefrog",
+                    pub: "pub-1",
+                    epub: "epub-1",
+                    picture: "",
+                } as Profile | FriendProfile,
+                "pub-2": {
+                    username: "polarbear",
+                    pub: "pub-2",
+                    epub: "epub-2",
+                    picture: "",
+                } as Profile | FriendProfile,
+            },
+        }, 
     },
     "groupid92": {
         groupId: stringToUint8Array("groupid92"),
@@ -20,4 +76,5 @@ const dummyGroups: { [groupId: string]: any } = {
         groupId: stringToUint8Array("groupid94"),
     },
 }
+
 export default dummyGroups
